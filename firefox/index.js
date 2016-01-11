ec570343af734ce2d6961de0afafc00a68899177
li1397937810 <li1397937810@gmail.com>
2016-01-11 17:42:15 +0800

    添加了显示面板

--- firefox/index.js
+++ firefox/index.js
@@ -1,17 +1,47 @@
 var buttons = require("sdk/ui/button/toggle");
 var self = require("sdk/self");
-require("sdk/tabs").on("ready",handleOpen);
 var tabs = require("sdk/tabs");
+var panels = require("sdk/panel");
+
+tabs.on("ready",handleOpen);
+
 
 
 var MonitorStatus = false;
+
+
+
+var panel = panels.Panel({
+  contentURL: self.data.url("menu.html"),
+  onHide: handleHide
+});
+
+
 
 var button = buttons.ToggleButton({
 	id : "status-switch",
 	label:"监视已关闭",
 	icon:"./tieba_disable.png",
-	onClick:handleClick
+	onClick:handleClick,
+	onChange:handleChange
 });
+
+
+
+
+function handleChange(state)
+{
+if (state.checked) {
+    panel.show({width: 180,
+      position: button
+    });
+  }
+}
+
+function handleHide() {
+  button.state('window', {checked: false});
+}
+
 
 function handleClick(status)
 {
@@ -19,7 +49,7 @@ function handleClick(status)
 	{
 
 		button.state("window",{
-		"label":"监视已开启",
+		"label":"监视已开启\n将自动监视页面！",
 		"icon":"./tieba_enable.png"
 		});
 	}
@@ -45,8 +75,6 @@ function handleOpen(tab)
 		
 	if(tab.url == "http://tieba.baidu.com/")
 	{
-	//button.state("tab",{		"label":"监视已开启",		"icon":"./tieba_enable.png"	});
-		//handleClick();
 		tab.attach({contentScriptFile:[self.data.url('jquery.js'),self.data.url('monitor.js')]
     //contentScript: "if (document.body) document.body.style.border = '5px solid red';"
   });
